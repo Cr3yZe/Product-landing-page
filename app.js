95d139e11042ab30644e108fcb13c78649392411
Cr3yZe <addy_czr@yahoo.com>
2022-03-13 15:01:08 +0200

    styling

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 const header = document.getElementById('header');
 
-header.addEventListener('click', function (e) {
-    let target = e.target;
+reloadPage();
+hiddeHeader();
 
-    if (target.classList.contains('logo') || target.classList.contains('title')) {
-        window.location.reload(true);
-    }    
-});
+function reloadPage () {
+    header.addEventListener('click', function (e) {
+        let target = e.target;
+    
+        if (target.classList.contains('logo') || target.classList.contains('title')) {
+            window.location.reload(true);
+        }    
+    });    
+}
+
+let lastScroll = window.scrollY;
+
+function hiddeHeader () {
+    window.addEventListener('scroll', function () {
+
+        if (lastScroll < window.scrollY) {
+            console.log(header.classList);
+            header.classList.add('remove');
+        } else {
+            header.classList.remove('remove');
+        }
+
+        lastScroll = window.scrollY;
+    })
+}
